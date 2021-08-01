3052a3449c3d9e15fc266d29f5000c4c9fcf7867
Afraz Malik <afrazmalik321@gmail.com>
2021-08-02 00:03:08 +0500

    Initializing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavbarCss from './Navbar.module.scss'
 
 const Navbar = () => {
+  const [nav, setNav] = useState({
+    search: false,
+    notification: false,
+    profile: false,
+  })
+  const toggleNav = (value) => {
+    if (value === 'notification') {
+      setNav({
+        ...nav,
+        notification: !nav.notification,
+      })
+    }
+    if (value === 'search') {
+      document.getElementById('input').style.display = 'block'
+      document.getElementById('input').focus()
+      setNav({
+        ...nav,
+        search: !nav.search,
+      })
+    }
+    if (value === 'closesearch') {
+      if (document.getElementById('input').value === '') {
+        document.getElementById('input').style.display = 'none'
+        console.log()
+        setNav({
+          ...nav,
+          search: !nav.search,
+        })
+      }
+    }
+    if (value === 'profile') {
+      setNav({
+        ...nav,
+        profile: !nav.profile,
+      })
+    }
+  }
+
   return (
     <div className={NavbarCss.navbar}>
       <div className={NavbarCss.leftnav}>
@@ -9,16 +47,41 @@ const Navbar = () => {
       </div>
       <div className={NavbarCss.rightnav}>
         <div className={NavbarCss.search}>
-          <img alt="" src="images/search.svg " />
+          <img
+            alt=""
+            src="images/search.svg "
+            onClick={() => toggleNav('search')}
+            style={nav.search ? { display: 'none' } : { display: 'block' }}
+          />
+          <input
+            type="text"
+            id="input"
+            onBlur={() => toggleNav('closesearch')}
+          />
         </div>
         <div className={NavbarCss.notification}>
-          <img alt="" src="images/notification.svg" />
+          <img
+            className={NavbarCss.notificationimg}
+            alt=""
+            src="images/notification.svg"
+            onClick={() => toggleNav('notification')}
+            onBlur={() => toggleNav('notification')}
+          />
           <img className={NavbarCss.dot} alt="" src="images/dot.svg" />
+          <div
+            className={NavbarCss.notificationBoxmodel}
+            style={
+              nav.notification ? { display: 'block' } : { display: 'none' }
+            }
+          >
+            <h2>No New Notification</h2>
+          </div>
         </div>
         <div className={NavbarCss.anime}>
           <img alt="" src="images/anime.png" />
         </div>
       </div>
+      {/* Their Box Models */}
     </div>
   )
 }
